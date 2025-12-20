d799cb6642ea42d5b9b863b7ce65425e6b38aa6d
Kyle Butler <theokyle@gmail.com>
2025-12-20 13:22:03 -0600

    seeded data and add expanded members controller

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,9 +7,12 @@ public class AppUser
     public required string DisplayName { get; set; }
 
     public required string Email { get; set; }
+    public string? ImageUrl { get; set; }
 
     public required byte[] PasswordHash { get; set; }
 
     public required byte[] PasswordSalt { get; set; }
+
+    public Member Member { get; set; } = null!;
 
 }
--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219fe0f546cecc799d924f9bfc457f05b4e8c63f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30d90cab79cc4f51e89ce0af2829b964ad48f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
