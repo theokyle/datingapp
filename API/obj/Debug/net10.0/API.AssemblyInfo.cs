e5b5c480181646622abf3384b4e854f07e0a17ad
Kyle Butler <theokyle@gmail.com>
2026-01-01 09:09:52 -0600

    add photo upload

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e04e215dc50196789c89b4fc744ab799733ec2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be8039dd55045975f478d174ccc6f4137dafc7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
